f00c11c921aadf642b96e12ad6f667198a262872
anastasiya.didych <didychanastasiya@gmailcom>
2022-04-26 00:28:45 +0300

    aadded logic for gravity and sound

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,39 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-	private Vector3 startPossition = new Vector3(-30, 2.77f, 0);
-	private Quaternion startRotation = new Quaternion(0, 0, 0, 0);
+
+	private int increment = 100;
 	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
 	[SerializeField]
-	private float upSpeed = 1;
+	private float upSpeed = 13;
 	[SerializeField]
-	private float moveSpeed = 1;
+	private float rotationSpeed = 1;
 
 	void Start() {
 		Debug.Log("Startig");
 		this.rigidBody = GetComponent<Rigidbody>();
+		this.audioSource= GetComponent<AudioSource>();
 	}
 
 	void Update() {
-		if (Input.GetKey(KeyCode.Space)) {
+		if (Input.GetKey(KeyCode.W)) {
+			if (!this.audioSource.isPlaying) {
+				this.audioSource.Play();
+			}
 			this.rigidBody.AddRelativeForce(getIndependentValue(Vector3.up, upSpeed));
-			Debug.Log("Performing jump");
+			Debug.Log("Moving up");
+		} else {
+			this.audioSource.Pause();
 		}
 
 		if (Input.GetKey(KeyCode.A)) {
-			this.transform.Rotate(getIndependentValue(Vector3.forward, moveSpeed));
+			this.transform.Rotate(getIndependentValue(Vector3.forward, rotationSpeed));
 			Debug.Log("Moving left");
 			return;
 		}
 
 		if (Input.GetKey(KeyCode.D)) {
-			this.transform.Rotate(getIndependentValue(Vector3.back, moveSpeed));
+			this.transform.Rotate(getIndependentValue(Vector3.back, rotationSpeed));
 			Debug.Log("Moving right");
 			return;
 		}
 	}
 
 	private Vector3 getIndependentValue(Vector3 value, float speed) {
-		return value * Time.deltaTime * speed;
+		return value * Time.deltaTime * (speed * increment);
 	}
 }
